3b5a881c00010135cd4e32b0554fa2c99c16e946
cmusman2 <cmusman@hotmail.com>
2017-05-01 18:44:42 +0100

    change

--- src/main/webapp/resources/js/controller/ItemListController.js
+++ src/main/webapp/resources/js/controller/ItemListController.js
@@ -8,7 +8,7 @@ App.controller('ItemListController', ['async', function(async) {
 App.controller('UtilityController', function ($scope){
    
 	this.doFormatDate=function(searchTerm){$scope.message=$('#CityAjaxH').val();}
-	this.deocdeHtml=function(searchTerm){$scope.deocdedHtml=" 	<strong>1 King Bed</strong><br />538-sq-foot (50-sq-meter) room w";}
+	this.toggleShowHide=function(sh){$scope.checked= !sh;}
      
 })
 .filter('trusted', ['$sce', function($sce){
